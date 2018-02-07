894ab20fd6d91348f6e1255f171d7cdb29adb60b
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 13:59:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def knowledge
+    @knowledge
+  end
+
+  def learn(string)
+    @knowledge << String
+  end
+  
 end
